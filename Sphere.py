97a84552ac63b0090a05ad5a50c7d57a8fe1dbd0
Fatemeh-ledari <fatemehzahra.asgharzadehledari@dawsoncollege.qc.ca>
2024-09-13 23:40:24 +0000

    Homework2

---
+++ Sphere.py
@@ -0,0 +1,9 @@
+def calculatevolume(radius):
+    volume = 4/3 * 3.14 * radius ** 3
+    return volume 
+
+Sphere1 = calculatevolume(4)
+print("The volume of the Sphere is:",Sphere1)
+
+Sphere2 = calculatevolume(20)
+print("The volume of the Sphere is:", Sphere2)
